923d4d8460bd993eb38f6d63c174a1bb8491b2f4
ziyi-bear <m0724001@gm.nuu.edu.tw>
2021-03-25 12:00:38 +0800

    init modify version

--- app/server.js
+++ app/server.js
@@ -3,13 +3,23 @@ var express = require('express');
 var app = express(); 						// create our app w/ express
 var mongoose = require('mongoose'); 				// mongoose for mongodb
 var port = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
+//var database = require('./config/database'); 			// load the database config
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
+
+
 // configuration ===============================================================
-mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
+// iiiorg RULE
+var db_username = process.env.db_username
+var db_password = process.env.db_password
+var db_host = process.env.db_host
+var db_name = process.env.db_name
+var database_url = 'mongodb://'+db_username+':'+db_password+'@'+db_host+':27017/'+db_name
+
+//mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
+mongoose.connect(database_url)
 
 app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
